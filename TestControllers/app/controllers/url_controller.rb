b3a35a8672d408613d7ffa551070849ed8734a03
Abhineesh Das <61410448@exchange.isb.edu>
2016-04-05 16:30:57 +0530

    Initial commit

---
+++ TestControllers/app/controllers/url_controller.rb
@@ -0,0 +1,20 @@
+class UrlController < ApplicationController
+  def new
+    @shortened_url = Url.new
+  end
+
+  def create
+    @shortened_url = Url.new(params[:url])
+    if @shortened_url.save
+      flash[:shortened_id]=@shortened_url.id
+      redirect_to_new_url_url
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @shortened_url= Url.find(params[:id])
+    redirect_to @shortened_url.url
+  end
+end
